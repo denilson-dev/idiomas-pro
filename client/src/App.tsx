9ee7c788de87b45f481d36f12bb22c65ae3c128e
Denilson Oliveira <144750433+denilson-dev@users.noreply.github.com>
2026-09-20 17:41:28 -0300

    feat: connect student setup and teacher portal routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,10 @@ import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
 import LanguageSelectPage from './pages/LanguageSelectPage';
 import ResultPage from './pages/ResultPage';
+import StudentSetupPage from './pages/StudentSetupPage';
+import TeacherDashboardPage from './pages/TeacherDashboardPage';
+import TeacherLoginPage from './pages/TeacherLoginPage';
+import TeacherStudentDetailPage from './pages/TeacherStudentDetailPage';
 import TestPage from './pages/TestPage';
 import WelcomePage from './pages/WelcomePage';
 
@@ -23,9 +27,15 @@ export default function App() {
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/language" element={<LanguageSelectPage />} />
+        <Route path="/setup" element={<StudentSetupPage />} />
         <Route path="/test" element={<TestPage />} />
         <Route path="/result/:attemptId" element={<ResultPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+
+        <Route path="/professor" element={<TeacherLoginPage />} />
+        <Route path="/professor/painel" element={<TeacherDashboardPage />} />
+        <Route path="/professor/aluno/:attemptId" element={<TeacherStudentDetailPage />} />
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
